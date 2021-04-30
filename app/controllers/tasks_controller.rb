0ea220854fe921994fb8036c1cc21bf604e76049
syougo <supernaturalsyougo@gmail.com>
2021-04-30 17:48:48 +0900

    ログイン機能の実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,36 @@
 class TasksController < ApplicationController
+  before_action :set_task, only:[:show,:edit,:update]
   def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      # @task = current_user.tasks.build(picture_params)
+      redirect_to task_path(@task.id), notice: 'picture page was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def confirm
+    @task = current_user.tasks.build(task_params)
+    render :new if @task.invalid?  
+  end
+  def update
+    if @task.update(task_params)
+      redirect_to tasks_path ,notice:'イベントを更新しまいた'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
   end
 
   def show
+
   end
 
   def edit
@@ -10,4 +38,13 @@ class TasksController < ApplicationController
 
   def index
   end
+
+  private
+  def set_task
+    @task = Task.find(params[:id])
+  end
+  def task_params
+    params.require(:task).permit(:title,:content)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
   resources :users
-  resources :sessions, only:[:new]
-  resources :tasks
+  resources :sessions, only: [:new, :create, :destroy]
   resources :labels
   resources :betweens, only:[:show]
   root to:'tasks#index'
+  resources :tasks do
+    collection do
+      post :confirm
+    end
+    end
 
 
 end
