941745f15d822f24a6d08f6c81cbb37eeea72982
syougo <supernaturalsyougo@gmail.com>
2021-05-05 17:13:39 +0900

    rspec機能修正

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,13 +1,16 @@
 FactoryBot.define do
   factory :task do
+    daytime {'002020-10-06'}
     title {'タスク'}
     content {'コンテント'}
   end
-  factory :task2 do
+  factory :task2,class: Task do
+    daytime {'002020-10-07'}
     title {'タスク2'}
     content {'コンテント2'}
   end
-  factory :task3 do
+  factory :task3,class: Task do
+    daytime {'002020-10-08'}
     title {'タスク3'}
     content {'コンテント3'}
   end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -3,19 +3,19 @@ describe 'タスクモデル機能', type: :model do
   describe 'バリデーションのテスト' do
     context 'タスクのタイトルが空の場合' do
       it 'バリデーションにひっかる' do
-        task = Task.new(title: '', content: '失敗テスト')
+        task = Task.new(daytime:'002020-10-06',title: '', content: '失敗テスト')
         expect(task).not_to be_valid
       end
     end
     context 'タスクの詳細が空の場合' do
       it 'バリデーションにひっかかる' do
-        task = Task.new(title:'失敗テスト',content:'')
+        task = Task.new(daytime:'002020-10-06',title:'失敗テスト',content:'')
         expect(task).not_to be_valid
       end
     end
     context 'タスクのタイトルと詳細に内容が記載されている場合' do
       it 'バリデーションが通る' do
-        task = Task.new(title:'成功テスト',content:'成功テスト')
+        task = Task.new(daytime:'002020-10-06',title:'成功テスト',content:'成功テスト')
         expect(task).to be_valid
       end
     end
