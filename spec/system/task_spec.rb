9e5b2892bbd4a6129fb05b59a58ff62a8a8ee9ea
syougo <supernaturalsyougo@gmail.com>
2021-05-08 11:47:23 +0900

    rspec修正と期限ソートテスト実装

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 RSpec.describe 'タスク管理機能', type: :system do
-  let!(:task) { FactoryBot.create(:task, title:'task',content: 'aaa',daytime:'002020-10-08')}
-  let!(:task2) { FactoryBot.create(:task2, title:'task2',content: 'bbb',daytime:'002020-10-08')}
-  let!(:task3) { FactoryBot.create(:task3, title:'task3',content: 'ccc',daytime:'002020-10-08')}
+  let!(:task) { FactoryBot.create(:task, title:'task',content: 'aaa',daytime:'002020-10-08',endtime_at:'002020-10-07')}
+  let!(:task2) { FactoryBot.create(:task2, title:'task2',content: 'bbb',daytime:'002020-10-08',endtime_at:'002020-10-06')}
+  let!(:task3) { FactoryBot.create(:task3, title:'task3',content: 'ccc',daytime:'002020-10-08',endtime_at:'002020-10-10')}
   describe '新規作成機能' do
     context 'タスクを新規作成した場合' do
       it '作成したタスクが表示される' do
@@ -11,6 +11,7 @@ RSpec.describe 'タスク管理機能', type: :system do
         fill_in 'task[title]', with: 'test_task55'
         # binding.irb
         fill_in 'task[content]', with: 'content_test55'
+        # fill_in 'task[endtime_at]', with: '002020-10-01'
         click_button '投稿する'
         # task = FactoryBot.create(:task,title: 'task')
         visit tasks_path
@@ -45,6 +46,14 @@ RSpec.describe 'タスク管理機能', type: :system do
         expect(task_list[0]).to have_content 'task3'
       end
     end
+    context 'タスク期限日時の降順に並んでいる場合' do
+      it 'タスクが期限日時の降順に並んでいること' do
+      visit tasks_path
+      click_on '期限並び替え'
+      task_list = all('.task_row')
+      expect(task_list[1]).to have_content '2020-10-07'
+    end
+   end
   end
   describe '詳細表示機能' do
      context '任意のタスク詳細画面に遷移した場合' do
