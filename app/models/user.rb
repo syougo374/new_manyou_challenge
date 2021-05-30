4b80182796e7b6d79e010fb8aa8ff0981e9e3d14
syougo <supernaturalsyougo@gmail.com>
2021-05-30 11:46:20 +0900

    質問用、Ajax実装するが動的にフォロー機能が動作しない

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,21 @@ class User < ApplicationRecord
   before_validation { email.downcase! }
   has_secure_password
   has_many :tasks, dependent: :destroy
+  has_many :active_relationships, foreign_key: 'follower_id', class_name: 'Relationship', dependent: :destroy
+  has_many :passive_relationships, foreign_key: 'followed_id', class_name: 'Relationship', dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
+  def follow!(other_user)
+    active_relationships.create!(followed_id: other_user.id)
+  end
+  #フォローしているかどうかを確認する
+  def following?(other_user)
+    active_relationships.find_by(followed_id: other_user.id)
+  end
+
+  def unfollow!(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
   enum admin: { 一般: false, 管理者: true }
   validates :password, presence: true, length: { minimum: 6 }
   validates :name, presence: true, length: { maximum: 10}
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+  get 'relationships/create'
+  get 'relationships/destroy'
   namespace :admin do
     resources :users
   end
   resources :users, only: [:index, :new, :create, :edit, :update, :show]
   resources :sessions, only: [:new, :create, :destroy]
+  resources :relationships, only: [:create, :destroy]
   # resources :labels
   # resources :betweens, only: [:create, :destroy, :show]
   root to:'tasks#index'
